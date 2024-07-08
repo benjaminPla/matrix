7185464f936f1423fbe74751c67aa4be86f5a556
benjaminPla <benjaminpla.dev@gmail.com>
2024-07-08 22:07:44 +0200

    update `fall` method, remove pointer from screen

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rand::{thread_rng, Rng};
 use std::io::{self, Write};
 extern crate termsize;
 
-#[derive(Debug)]
+// #[derive(Debug)]
 struct Char {
     row: u16,
     col: u16,
@@ -12,11 +12,15 @@ struct Char {
 }
 
 impl Char {
-    fn fall(&mut self, rows: u16, mut rng: ThreadRng) {
+    fn fall(&mut self, bottom: u16, cols: u16, mut rng: ThreadRng) {
         let ch: char = rng.sample(Alphanumeric).into();
         self.ch = ch;
-        match self.row == rows {
-            true => self.row = 1,
+        match self.row == bottom {
+            true => {
+                let random_col: u16 = rng.gen_range(0..=cols + 1);
+                self.col = random_col;
+                self.row = 0
+            }
             false => self.row += 1,
         }
     }
@@ -24,23 +28,27 @@ impl Char {
 
 fn main() {
     print!("{}[2J", 27 as char);
+    print!("\x1B[?25l");
 
     let mut rng = thread_rng();
     let term_size = termsize::get().unwrap();
+    let max_chars: u64 = (term_size.cols as f32 * 0.3) as u64;
 
     let mut chars: Vec<Char> = Vec::new();
 
     loop {
-        let random_col: u16 = rng.gen_range(0..term_size.cols);
-        let ch: char = rng.sample(Alphanumeric).into();
-        chars.push(Char {
-            row: 0,
-            col: random_col,
-            ch,
-        });
+        if chars.len() < max_chars as usize {
+            let random_col: u16 = rng.gen_range(0..=term_size.cols + 1);
+            let ch: char = rng.sample(Alphanumeric).into();
+            chars.push(Char {
+                row: 0,
+                col: random_col,
+                ch,
+            });
+        }
 
         for ch in chars.iter_mut() {
-            ch.fall(term_size.rows, rng.clone());
+            ch.fall(term_size.rows, term_size.cols, rng.clone());
             print!("\x1B[32m\x1B[{};{}H{}\x1B[0m", ch.row, ch.col, ch.ch);
         }
 
